ee4c09393885b3c35294b85535b5a29ad6e566ca
vkumar25 <Vineet_Kumar@intuit.com>
2019-04-18 12:23:20 +0530

    Initial changes

---
+++ src/main/java/com/easysale/configserver/EasySaleConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.easysale.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class EasySaleConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasySaleConfigServerApplication.class, args);
+	}
+
+}
